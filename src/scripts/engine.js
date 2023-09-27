ce3f94117cb50ac3a965b97cc46424e55a18daca
LucasNunes <lucasfnunes6@gmail.com>
2023-09-27 15:16:29 -0300

    Adicionando efeito rotação nos cards

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -20,10 +20,26 @@ let openCards = [];
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
+function checkMatch() {
+    
+}
+
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if (openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
 for(let i=0; i <emojis.length; i++) 
 {
     let box = document.createElement("div");
     box.classList = "item";
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
 }
